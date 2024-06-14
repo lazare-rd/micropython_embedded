384e7fc9244b9d8ff0ff116f9264110f44708c87
Lazare Ricour-Dumas <lazare.rd@gmail.com>
2024-06-14 11:55:06 +0200

    un peu de ménage

--- micropython_embed/main.c
+++ micropython_embed/main.c
@@ -6,27 +6,6 @@
 #include "port/micropython_embed.h"
 #include "python_script.h"
 
-// This is example 1 script, which will be compiled and executed.
-static const char *example_1 =
-    "print('hello world!', list(x + 1 for x in range(10)), end='eol\\n')";
-
-// This is example 2 script, which will be compiled and executed.
-static const char *example_2 =
-    "for i in range(10):\n"
-    "    print('iter {:08}'.format(i))\n"
-    "\n"
-    "try:\n"
-    "    1//0\n"
-    "except Exception as er:\n"
-    "    print('caught exception', repr(er))\n"
-    "\n"
-    "import gc\n"
-    "print('run GC collect')\n"
-    "gc.collect()\n"
-    "\n"
-    "print('finish')\n"
-    ;
-
 // This array is the MicroPython GC heap.
 static char heap[8 * 1024];
 
